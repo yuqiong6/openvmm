9c14a7a1a7be2ef9202ec1dade5796e0110c9510
Yuqiong <yuqliu@microsoft.com>
2026-04-22 11:29:20 -0700

    Enable MANA keep-alive by default

--- openhcl/underhill_core/src/dispatch/mod.rs
+++ openhcl/underhill_core/src/dispatch/mod.rs
@@ -684,9 +684,11 @@ impl LoadedVm {
             self.nvme_keep_alive = KeepAliveConfig::Disabled
         };
 
-        if !capabilities_flags.enable_mana_keepalive() {
-            self.mana_keep_alive = KeepAliveConfig::Disabled
-        };
+        // HARDCODED: Always enable mana keepalive for internal testing
+        //if !capabilities_flags.enable_mana_keepalive() {
+        //    self.mana_keep_alive = KeepAliveConfig::Disabled
+        //};
+        self.mana_keep_alive = KeepAliveConfig::EnabledHostAndPrivatePoolPresent;
 
         // Do everything before the log flush under a span.
         let r = async {
--- openhcl/underhill_core/src/worker.rs
+++ openhcl/underhill_core/src/worker.rs
@@ -3416,7 +3416,9 @@ async fn new_underhill_vm(
                     &vmbus_server,
                     dma_manager.client_spawner(),
                     isolation.is_isolated(),
-                    env_cfg.mana_keep_alive.clone(),
+                    //env_cfg.mana_keep_alive.clone(),
+                    // HARDCODED: Always enable mana keepalive for internal testing
+                    KeepAliveConfig::EnabledHostAndPrivatePoolPresent,
                     nic_servicing_state,
                 )
                 .await?;
@@ -3635,7 +3637,9 @@ async fn new_underhill_vm(
 
         _periodic_telemetry_task: periodic_telemetry_task,
         nvme_keep_alive: env_cfg.nvme_keep_alive,
-        mana_keep_alive: env_cfg.mana_keep_alive,
+        //mana_keep_alive: env_cfg.mana_keep_alive,
+        // HARDCODED: Always enable mana keepalive for internal testing
+        mana_keep_alive: KeepAliveConfig::EnabledHostAndPrivatePoolPresent,
         test_configuration: env_cfg.test_configuration,
         dma_manager,
         config_timeout_in_seconds: env_cfg.config_timeout_in_seconds,
